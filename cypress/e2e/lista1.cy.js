7ae1b077e72bf98929e949f75d13df766ae915f1
LeoTeodoro <leoteodoro18@gmail.com>
2024-10-25 01:10:39 -0300

    Configuração do cypress e do ambiente

---
+++ cypress/e2e/lista1.cy.js
@@ -0,0 +1,10 @@
+/// <reference = cypress>
+
+describe('Testes de login no site de uma empresa aérea',()=>{
+    it('Teste de cadastro com sucesso',()=>{
+
+    }),
+    it('Teste de cadastro com erro',()=>{
+        
+    })
+})
