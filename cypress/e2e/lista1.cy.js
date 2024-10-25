4efb57b3ba0dd3750c785abe5e0519153f10036e
LeoTeodoro <leoteodoro18@gmail.com>
2024-10-25 10:39:09 -0300

    testes da lista 1

--- cypress/e2e/lista1.cy.js
+++ cypress/e2e/lista1.cy.js
@@ -1,10 +1,43 @@
 /// <reference = cypress>
 
-describe('Testes de login no site de uma empresa aérea',()=>{
-    it('Teste de cadastro com sucesso',()=>{
+describe('Testes de cadastro e login no site de uma empresa de serviços aéreos',()=>{
+    it('Teste de login sem senha',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('input[name="username"]').type('Admin');
+        cy.get('button[type="submit"]').click();
+        cy.get('span[class*="error-message"]').should('have.text','Required');
 
     }),
-    it('Teste de cadastro com erro',()=>{
+    it('Teste de login sem username',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('input[name="password"]').type('admin123');
+        cy.get('button[type="submit"]').click();
+        cy.get('span[class*="error-message"]').should('have.text','Required');
+
+    }),
+    it('Teste de login vazio',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('button[type="submit"]').click();
+        cy.get('span[class*="error-message"]').should('be.visible');
+
+    }),
+    it('Teste de login válido',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('input[name="username"]').type('Admin');
+        cy.get('input[name="password"]').type('admin123');
+        cy.get('button[type="submit"]').click();
+        cy.get('h6[class*="topbar"]').should('have.text','Dashboard');
         
+    }),
+    it('Teste de recuperação de senha sem username',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode');
+        cy.get('button[type="submit"]').click();
+        cy.get('span[class*="error-message"]').should('have.text','Required');
+    }),
+    it('Teste de recuperação de senha válido',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode');
+        cy.get('input[name="username"]').type('Admin');
+        cy.get('button[type="submit"]').click();
+        cy.get('h6[class*="forgot-password"]').should('have.text','Reset Password link sent successfully');
     })
 })
